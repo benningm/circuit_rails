632fae419f814481ebf53beae73b89e013b236b0
Markus Benning <markus.benning@atos.net>
2018-02-26 13:02:09 +0100

    Revert direct registering of delivery_method

--- lib/circuit_rails/railtie.rb
+++ lib/circuit_rails/railtie.rb
@@ -1,7 +1,9 @@
 module CircuitRails
   class Railtie < Rails::Railtie
     initializer "circuit_rails.add_delivery_method" do
-      ActionMailer::Base.add_delivery_method :circuit, CircuitRails::DeliveryMethod
+      ActiveSupport.on_load :action_mailer do
+        ActionMailer::Base.add_delivery_method :circuit, CircuitRails::DeliveryMethod
+      end
     end
   end
 end
