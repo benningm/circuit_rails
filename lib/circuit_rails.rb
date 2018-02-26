2edb3d7df251f23c4233421d89d18dccc9a47792
Markus Benning <markus.benning@atos.net>
2018-02-26 12:45:44 +0100

    Load railtie only when "on rails"

--- lib/circuit_rails.rb
+++ lib/circuit_rails.rb
@@ -1,7 +1,8 @@
 
 require 'circuit_rails/mail_ext'
 require 'circuit_rails/delivery_method'
-require 'circuit_rails/railtie'
+
+require 'circuit_rails/railtie' if defined?(Rails)
 
 module CircuitRails
 end
